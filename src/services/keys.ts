ee80dcd7cdcb55e4ae54aa4ca33d79d1710ea88d
Ranjith Kumar R G <theranjith@gmail.com>
2026-02-15 12:22:20 +0530

    added userkey and added serialize function

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -1,1 +1,2 @@
 export const pageCacheKey = (id: string) => `pagecache#${id}`;
+export const userskey = (userId: string) => `users#${userId}`;
--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -1,8 +1,21 @@
 import type { CreateUserAttrs } from '$services/types';
 import { genId } from '$services/utils';
+import { client } from '$services/redis';
+import { userskey } from '$services/keys';
 
 export const getUserByUsername = async (username: string) => {};
 
 export const getUserById = async (id: string) => {};
 
-export const createUser = async (attrs: CreateUserAttrs) => {};
+export const createUser = async (attrs: CreateUserAttrs) => {
+	const id = genId();
+	await client.hSet(userskey(id), serialize(attrs));
+	return id;
+};
+
+const serialize = (user: CreateUserAttrs) => {
+	return {
+		userName: user.username,
+		password: user.password
+	};
+};
