6949eb36eaa3482fecf33dd271aee5a7f75bb2c7
Ranjith Kumar R G <theranjith@gmail.com>
2026-02-15 12:32:18 +0530

    fetching user

--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -5,7 +5,11 @@ import { userskey } from '$services/keys';
 
 export const getUserByUsername = async (username: string) => {};
 
-export const getUserById = async (id: string) => {};
+export const getUserById = async (id: string) => {
+	const user = await client.hGetAll(userskey(id));
+
+	return deserialize(id, user);
+};
 
 export const createUser = async (attrs: CreateUserAttrs) => {
 	const id = genId();
@@ -16,6 +20,14 @@ export const createUser = async (attrs: CreateUserAttrs) => {
 const serialize = (user: CreateUserAttrs) => {
 	return {
 		userName: user.username,
+		password: user.password
+	};
+};
+
+const deserialize = (id: string, user: { [key: string]: string }) => {
+	return {
+		id,
+		username: user.username,
 		password: user.password
 	};
 };
